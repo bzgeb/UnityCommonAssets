ab21e9e40b130b41b24bfee19b6ec1de1f0b5a82
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-04-01 14:04:31 -0400

    Added scale sin script

---
+++ Scripts/ScaleSin.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ScaleSin : MonoBehaviour
+{
+    public Vector3 minScale;
+    public Vector3 maxScale;
+    public float rate;
+
+    Transform _transform;
+
+    void Update() {
+        if ( _transform == null ) {
+            _transform = GetComponent<Transform>();
+        }
+        float lerpValue = ( Mathf.Sin( Time.timeSinceLevelLoad * rate ) + 1 ) * 0.5f;
+
+        _transform.localScale = Vector3.Lerp( minScale, maxScale, lerpValue );
+    }
+}
