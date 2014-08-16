53823512e644186f74636a4e070ff526c7f5b4f0
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-08-16 13:53:56 -0400

    Re-organization

---
+++ Scripts/EventManager/SendMessageOnEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendMessageOnEvent : MonoBehaviour 
+{
+    public Events eventName;
+    public GameObject target;
+    public string message;
+
+    void OnEnable() {
+        EventManager.Register( eventName.ToString(), OnEvent );
+    }
+
+    void OnDisable() {
+        EventManager.Deregister( eventName.ToString(), OnEvent );
+    }
+
+    void OnEvent( params object[] args ) {
+        if ( target != null ) {
+            target.SendMessage( message );
+        } else {
+            SendMessage( message );
+        }
+    }
+}
