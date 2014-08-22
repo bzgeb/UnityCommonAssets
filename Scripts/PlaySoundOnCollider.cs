dc5a74e812f5d48627472da47670370e07344ef4
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-08-21 20:04:38 -0400

    Play sound on collider or trigger scripts

---
+++ Scripts/PlaySoundOnCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnCollider : MonoBehaviour {
+    public bool playOnlyOnce = false;
+    bool played = false;
+    
+    public void OnCollisionEnter( Collision collision ) {
+        if ( !played ) {
+            audio.Play();
+            played = true;
+        }
+
+    }
+}
---
+++ Scripts/PlaySoundOnTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundOnTrigger : MonoBehaviour {
+    public bool playOnlyOnce = false;
+    bool played = false;
+
+    public void OnTriggerEnter( Collider other ) {
+        if ( !played ) {
+            audio.Play();
+            played = true;
+        }
+    }
+}
