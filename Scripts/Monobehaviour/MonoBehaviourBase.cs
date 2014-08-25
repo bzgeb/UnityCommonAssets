fcf50f89edae45a36f1a63ae9b88abaddccddf2f
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-08-25 12:31:04 -0400

    Added some basic monobehaviour extensions

---
+++ Scripts/Monobehaviour/MonoBehaviourBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonoBehaviourBase : MonoBehaviour {
+    public delegate void Task();
+
+    public void Invoke(Task task, float time)
+    {
+       Invoke(task.Method.Name, time);
+    }
+}
