0dc804e07d8b53f59d50a28729a7ab10b8be1ad1
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-08-25 13:14:02 -0400

    Added a dumb recursive find

--- Scripts/Monobehaviour/MonoBehaviourBase.cs
+++ Scripts/Monobehaviour/MonoBehaviourBase.cs
@@ -4,8 +4,29 @@ using System.Collections;
 public class MonoBehaviourBase : MonoBehaviour {
     public delegate void Task();
 
-    public void Invoke(Task task, float time)
-    {
+    public void Invoke(Task task, float time) {
        Invoke(task.Method.Name, time);
+    }
+
+    public Transform FindInChildren( Transform t, string name ) {
+        Transform r = null;
+
+        foreach ( Transform child in t ) {
+            if ( child.name.ToLower() == name.ToLower() ) {
+                r = child;
+                break;
+            }
+        }
+
+        if ( r == null ) {
+            foreach ( Transform child in t ) {
+                r = FindInChildren( child, name );
+                if ( r != null ) {
+                    break;
+                }
+            }
+        }
+
+        return r;
     }
 }
