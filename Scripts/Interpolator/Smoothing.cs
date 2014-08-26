6ddd5c818d47b333270059a8b3fdcfc17377ca22
Bronson Zgeb <bronson.zgeb@gmail.com>
2014-08-26 11:47:23 -0400

    Moved smoothing into common

---
+++ Scripts/Interpolator/Smoothing.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct SmoothingData {
+    public float value;
+    public float target;
+    public float strength;
+
+    public SmoothingData( float target = 0, float strength = 1, float value = 0 ) {
+        this.target = target;
+        this.strength = strength;
+        this.value = value;
+    }
+}
+
+static public class Smoothing 
+{
+    static public float Exponential( SmoothingData smoothingData ) {
+        return smoothingData.value + ( smoothingData.target - smoothingData.value ) * 0.01f;
+    }
+
+    static public float LowPass( SmoothingData smoothingData ) {
+        return smoothingData.value + ( smoothingData.target - smoothingData.value ) * smoothingData.strength;
+    }
+}
