c2e500468b8739748412033c1045261040f49562
Bronson Zgeb <bronson.zgeb@gmail.com>
2015-04-20 17:59:28 -0400

    beep boop

---
+++ Scripts/Interpolator/AnimatorInterpolator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatorInterpolator : MonoBehaviour {
+    public Animator animator;
+    public Interpolator interpolator;
+    public string stateName;
+    public int layer;
+
+    void Start() {
+        if ( animator == null ) {
+            Debug.LogError( "No animator found" );
+            enabled = false;
+        }
+    }
+
+    void SampleAnimation() {
+        animator.Play( stateName, layer, interpolator.Value );
+    }
+
+    void Update() {
+        SampleAnimation();
+    }
+}
