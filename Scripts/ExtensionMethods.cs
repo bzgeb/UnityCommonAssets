a46c83e10832a3c7d60d887eaea01d716103665e
Bronson Zgeb <bronson.zgeb@gmail.com>
2015-05-13 16:51:54 -0400

    Added an extension for getting the path of a transform

--- Scripts/ExtensionMethods.cs
+++ Scripts/ExtensionMethods.cs
@@ -1,5 +1,15 @@
-﻿public static class ExtensionMethods {
+﻿using UnityEngine;
+
+public static class ExtensionMethods {
     public static float Remap (this float value, float from1, float to1, float from2, float to2) {
         return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+    public static string GetPath(this Transform current) {
+         if (current.parent == null) {
+             return "/" + current.name;
+         }
+
+         return current.parent.GetPath() + "/" + current.name;
     }
 }
