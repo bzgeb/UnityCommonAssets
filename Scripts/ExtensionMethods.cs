e3293a846e590b9df326484d189bff41865c9bbe
Bronson Zgeb <bronson.zgeb@gmail.com>
2015-05-27 18:07:36 -0400

    Extension methods to copy components

--- Scripts/ExtensionMethods.cs
+++ Scripts/ExtensionMethods.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System;
+using System.Reflection;
 
 public static class ExtensionMethods {
     public static float Remap (this float value, float from1, float to1, float from2, float to2) {
@@ -31,5 +33,31 @@ public static class ExtensionMethods {
         }
 
         return current.parent.ParentCount() + 1;
+    }
+
+    public static T GetCopyOf<T>(this Component comp, T other) where T : Component
+    {
+        Type type = comp.GetType();
+        if (type != other.GetType()) return null; // type mis-match
+        BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+        PropertyInfo[] pinfos = type.GetProperties(flags);
+        foreach (var pinfo in pinfos) {
+            if (pinfo.CanWrite) {
+                try {
+                    pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
+                }
+                catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+            }
+        }
+        FieldInfo[] finfos = type.GetFields(flags);
+        foreach (var finfo in finfos) {
+            finfo.SetValue(comp, finfo.GetValue(other));
+        }
+        return comp as T;
+    }
+
+    public static T AddComponent<T>(this GameObject go, T toAdd) where T : Component
+    {
+        return go.AddComponent<T>().GetCopyOf(toAdd) as T;
     }
 }
